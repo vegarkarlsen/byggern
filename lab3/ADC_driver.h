81ea5e73492c86288ad894103e53b2a189f150d1
Vegar Karlsen <vegar@live.no>
2023-09-19 18:58:45 +0200

    finsihed lab3, and almost lab4

--- lab3/ADC_driver.h
+++ lab3/ADC_driver.h
@@ -1,7 +1,25 @@
 
 #pragma once
-#include <avr/io>
+#include <avr/io.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+#define ADC_START_ADRESS 0x1400
+
+void ADC_timer_init();
 
 
-void ADC_init();
+uint8_t read_channel(uint8_t channel);
+
+
+enum x {NONE=0, UP=1, DOWN=2, LEFT=3, RIGHT=4};
+struct Position {
+    uint8_t x;
+    uint8_t y;
+};
+
+
+
+struct Position calculate_direction_joy(int8_t percentage_x, int8_t percentage_y);
+
+int8_t analog_to_persentage_joy(uint8_t input, int8_t offset);
--- lab3/main.c
+++ lab3/main.c
@@ -1,68 +1,65 @@
 
-#define FOSC 4915200 // Clock Speed
-#define BAUD 9600
-#define MYUBRR FOSC/16/BAUD-1
-#define F_CPU 16000000
-
-
 #include <avr/io.h>
 #include "USART_driver.h"
 #include <stdio.h>
 #include <util/delay.h>
 #include "SRAM_test.h"
+#include "ADC_driver.h"
+#include "util.h"
+#include <avr/interrupt.h>
+#include <stdbool.h>
 
+#define FOSC 4915200 // Clock Speed
+#define BAUD 9600
+#define MYUBRR FOSC/16/BAUD-1
+#define F_CPU 16000000
 
-void test_hardcode(){
-    volatile uint8_t *sramAdress = (uint8_t *) 0x1802;
-    volatile uint8_t *adcAdress = (uint8_t *) 0x1405;
+uint8_t joy_x = -1;
+uint8_t joy_y = -1;
 
-    printf("starting sram \r\n");
-    for (int i = 0; i < 255; i++){
-        *sramAdress = i;
-        printf("sram: %4d \r\n", *sramAdress);
-    }
-    _delay_ms(1000);
+uint8_t slider_left = -1;
+uint8_t slider_right = -1;
 
-    printf("starting ADC \r\n");
-    for (int i = 0; i < 255; i++){
-        *adcAdress = i;
-        printf("adc: %4d \r\n", *adcAdress);
-    }
-}
-
-
-
-
-
-void read_ch_1(){
-    volatile char *adc = (char *) 0x1400;
-    adc[0] = 0x00;
-    _delay_ms(50);
-    uint8_t value = adc[0];
-    printf("adc: %0X\r\n", value);
-     
-}
-
-void eneable_adc_timer(){
-    DDRD |= (1 << PD4);
-    TCCR1A = (1 << COM1A0);
-    TCCR1B = (1 << WGM12) | (1 << CS10);
-    OCR0 = 1;
-}
+bool p0 = 0;
 
 
 int main(void)
 {
     USART0_init( MYUBRR );
-    SRAM_init();
-    
-    eneable_adc_timer();
-
+    SRAM_init(); // this is genreal for databus
     // SRAM_test();
-    
-    // test_hardcode();
-	while (1){
+    ADC_timer_init();
 
-        read_ch_1();
+
+    // set up buttons as read
+    // DDRB |= (1 << PB0) | (1 << PB1) | (1 << PB2);
+    
+
+    
+
+	while (1){
+        joy_x = read_channel(0);
+        joy_y = read_channel(1);
+        
+        slider_left = read_channel(2);
+        slider_right = read_channel(3);
+
+        // int8_t joy_x_per = map(joy_x, 0, 255, 100, -100);
+        // int8_t joy_y_per = map(joy_y, 0, 255, 100, -100);
+
+        // struct Position pos = calculate_direction_joy(joy_x_per, joy_y_per);
+
+        int8_t joy_x_per = analog_to_persentage_joy(joy_x, 3);
+        int8_t joy_y_per = analog_to_persentage_joy(joy_y, 3);
+        uint8_t p0 = PINB >> 0;
+        
+
+
+        printf("joysick (x, y): (%2d, %2d) \r\n",joy_x_per, joy_y_per);
+        // printf("joysick (x, y): (%2d, %2d) \r\n",joy_x, joy_y);
+        // printf("joysick (x, y): (%2d, %2d) \r\n",pos.x, pos.y);
+        printf("slider (LEFT, RIGHT): (%2d, %2d) \r\n", slider_left, slider_right);
+
+
 	}
 }
