3fa10d4e4f2009278bc3d0cdc8c82a8730d0cf5f
Vegar Karlsen <vegar@live.no>
2023-09-27 13:40:05 +0200

    mv .c to src and .h to include

--- lab4/dataBus.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <avr/io.h>
-#include <stdlib.h>
-
-
-#define SRAM_START_ADRESS 0x1800
-#define ADC_START_ADRESS 0x1400
-#define OLED_DATA_START_ADRESS 0x1200
-#define OLED_COMMAND_START_ADRESS 0x1000
