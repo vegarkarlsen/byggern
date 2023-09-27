52c1d86429e8f702022de9beaec9a43d4571f9e7
Vegar Karlsen <vegar@live.no>
2023-09-27 15:33:45 +0200

    l

--- lab4/util.c
+++
@@ -1,7 +0,0 @@
-
-#include "util.h"
-#include <stdint.h>
-
-long map(long input, long i_min, long i_max, long o_max, long o_min) {
-    return (input - i_min) * (o_max - o_min) / (i_max - i_min) + o_min;
-}
