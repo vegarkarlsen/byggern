9469627b0e8252f76f08e3afc1b5a4fca05cac3c
Vegar Karlsen <vegar@live.no>
2023-10-03 17:39:59 +0200

    SPI driver

---
+++ include/SPI_driver.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <avr/io.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <util/delay.h>
+
+#define DDR_SPI DDRB
+#define DD_SS PB4
+#define DD_MOSI PB5
+#define DD_MISO PB6 
+#define DD_SCK PB7 
+
+/* init SPI, set ATMega as master */
+void SPI_master_init();
+
+// transmitt data to slave(s) MOSI
+void SPI_transmit(uint8_t byte);
+
+// read value from MISO
+uint8_t SPI_revice();
+
+// revice and transmitt in the same time (less resources?)
+uint8_t SPI_transmit_and_recice(uint8_t byte);
+
+// set chicp select
+void SPI_set_ss();
+
+// reset chicp select 
+void SPI_reset_ss();
