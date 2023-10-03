499b2b3df05defd22457634642a2fc9053ee99f1
Vegar Karlsen <vegar@live.no>
2023-10-03 17:39:08 +0200

    misc

--- include/util.h
+++ include/util.h
@@ -1,5 +1,12 @@
 #pragma once
 #include <avr/io.h>
+/* #include <cstdint> */
+/* #include <cstdint> */
 #include <stdlib.h>
 
 long map(long input, long i_min, long i_max, long o_max, long o_min);
+
+// TODO: fix this functions
+void set_bit(uint8_t *byte, uint8_t bit);
+
+void clear_bit(char *byte, uint8_t bit);
--- src/util.c
+++ src/util.c
@@ -5,3 +5,8 @@
 long map(long input, long i_min, long i_max, long o_max, long o_min) {
     return (input - i_min) * (o_max - o_min) / (i_max - i_min) + o_min;
 }
+
+
+void set_bit(uint8_t *byte, uint8_t bit){
+   *byte = (1 << bit);
+}
